01cbf080623050d3d1f3775c013c76914c6c9c7f
Alexey Uzlov <auzlov@codemastersintl.com>
2022-10-24 19:51:24 +0400

    * code review

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const nav = document.querySelector('#nav');
 const navBtn = document.querySelector('#nav__button');
 const navBtnImg = document.querySelector('#nav__button-img');
 
+// ID не используют для js, также используют классы
 navBtn.onclick = () => {
     if (nav.classList.toggle('nav__list--open')) {
         navBtnImg.src = "./img/nav-close.svg";
