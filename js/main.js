8d45389cb7f940955a47e632968cf76b41dbacaf
va1ce <mr.vusu222@mail.ru>
2022-10-27 15:35:18 +0300

    fixes pull request

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const nav = document.querySelector('.navigation-list');
 const navBtn = document.querySelector('.nav__button');
 const navBtnImg = document.querySelector('.fa-bars');
 
+// ID не используют для js, также используют классы
 navBtn.onclick = () => {
     if (nav.classList.toggle('navigation-list--open')) {
         navBtnImg.className = "fa-solid fa-xmark fa-2x";
